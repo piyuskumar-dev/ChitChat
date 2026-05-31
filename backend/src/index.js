af04ae5954e7d1dd58676a745deb4a22ec32575c
Piyus Kumar <piyus_device@Piyuss-MacBook-Air.local>
2026-05-31 22:33:22 +0530

    ready for production

--- backend/src/index.js
+++ backend/src/index.js
@@ -33,7 +33,9 @@ app.use(
   })
 );
 
-
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+})
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/groups", groupRoutes);
