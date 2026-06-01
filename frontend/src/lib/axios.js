58d757fe0df0a04171fee78c30057c7911a7f448
Piyus Kumar <piyus_device@Piyuss-MacBook-Air.local>
2026-06-01 21:22:28 +0530

    axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? (import.meta.env.VITE_BACKEND_URL || "http://localhost:5001") + "/api" : "/api",
+  baseURL: (import.meta.env.VITE_BACKEND_URL || "http://localhost:5001") + "/api",
   withCredentials: true,
 });
